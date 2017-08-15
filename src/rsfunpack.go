48e5a51e9add1befdab88d80f131ab6cabb1e582
Matthew Shaw <mtthwjhnshw@gmail.com>
2017-08-15 13:23:57 +0800

    Added .BMP handling

--- src/rsfunpack.go
+++ src/rsfunpack.go
@@ -8,22 +8,38 @@ import (
 	"os"
 )
 
+type BitmapHeader struct {
+	HeaderField uint16
+	Size uint32
+	Res1 uint16
+	Res2 uint16
+	DataAddress uint32
+}
+
+type BitmapDIB struct {
+	DIBSize uint32
+	Width uint16
+	Height uint16
+	ColPlanes uint16
+	Bpp uint16
+}
+
 type Header struct {
-	License     [100]byte
+	License     [100]byte 
 	Name        [12]byte
 	Version     [8]byte
 	Timestamp   [42]byte
 	FileSize    uint32
-	FormatCount uint16
+	DirectoryCount uint16
 	FileCount   uint16
-	Val1       [2]byte 
-	Val2       [2]byte 
-	Val3       [2]byte 
-	Val4       [2]byte 
-	Val5       [2]byte 
+	Val1       [2]byte //unidentified
+	Val2       [2]byte //unidentified
+	Val3       [2]byte //unidentified
+	Val4       [2]byte //unidentified
+	Val5       [2]byte //unidentified
 }
 
-type FormatInfo struct {
+type DirectoryInfo struct {
 	Name  [4]byte
 	Count uint16
 	Pos   uint16
@@ -34,7 +50,7 @@ type FileInfo struct {
 	_    uint16
 	Size uint32
 	Addr uint32
-	Val1 uint32
+	Val1 uint32 //unidentified
 }
 
 func check(e error) {
@@ -43,9 +59,8 @@ func check(e error) {
 	}
 }
 
-func readNextBytes(file *os.File, number int) []byte {
+func readNumBytes(file *os.File, number int) []byte {
 	bytes := make([]byte, number)
-
 	num, err := file.Read(bytes)
 	if num != number {
 		fmt.Printf("Ran out of bytes! (wanted: %d, got: %d)\n", number, num)
@@ -55,7 +70,7 @@ func readNextBytes(file *os.File, number int) []byte {
 }
 
 func getBuffer(f *os.File, n int) *bytes.Buffer {
-	data := readNextBytes(f, n)
+	data := readNumBytes(f, n)
 	buffer := bytes.NewBuffer(data)
 	return buffer
 }
@@ -67,55 +82,122 @@ func unpackHeader(f *os.File, hdrSize int) *Header {
 	return &hdr
 }
 
-func unpackFormatList(f *os.File, hdr *Header) []*FormatInfo {
-	formatList := make([]*FormatInfo, hdr.FormatCount)
-	for i := 0; i < int(hdr.FormatCount); i++ {
-		aFormat := FormatInfo{}
-		err := binary.Read(getBuffer(f, 8), binary.LittleEndian, &aFormat)
+func unpackDirectoryList(f *os.File, cnt int) []*DirectoryInfo {
+	dir_list := make([]*DirectoryInfo, cnt)
+	for i := 0; i < cnt; i++ {
+		dir := DirectoryInfo{}
+		err := binary.Read(getBuffer(f, 8), binary.LittleEndian, &dir)
 		check(err)
-		formatList[i] = &aFormat
+		dir_list[i] = &dir
 	}
-	return formatList
+	return dir_list
 }
 
-func unpackFileList(f *os.File, hdr *Header) []*FileInfo {
-	fileList := make([]*FileInfo, hdr.FileCount)
-	for i := 0; i < int(hdr.FileCount); i++ {
-		aFile := FileInfo{}
-		err := binary.Read(getBuffer(f, 26), binary.LittleEndian, &aFile)
+func unpackFileList(f *os.File, cnt int) []*FileInfo {
+	file_list := make([]*FileInfo, cnt)
+	for i := 0; i < cnt; i++ {
+		file := FileInfo{}
+		err := binary.Read(getBuffer(f, 26), binary.LittleEndian, &file)
 		check(err)
-		fileList[i] = &aFile
+		file_list[i] = &file
 	}
-	return fileList
+	return file_list
 }
 
-func unpackFiles(f *os.File, hdr *Header, formats []*FormatInfo, files []*FileInfo)  {	
-	for i := 0; i < len(formats); i++ {
-		workDir := "./" +  string(bytes.Trim(hdr.Name[:12], "x\000")) + "/" +
-			string(formats[i].Name[:3]) + "/"
-		fmt.Printf("Extracting to %s\n", workDir)
-		os.MkdirAll(workDir, os.ModePerm)
-		fmt.Printf("File count: %d\n", formats[i].Count)
-		for ii := formats[i].Pos; ii < formats[i].Count+formats[i].Pos; ii++ {
-			s := workDir + string(bytes.Trim(files[ii].Name[:12], "x\000"))
-			//fmt.Printf("Writing file: %s\n", s)
-			w, werr := os.Create(s)
-			check(werr)
-			addr := int64(files[ii].Addr)
-			fsize := int(files[ii].Size)
-			f.Seek(addr, 0)
-			wdata := (readNextBytes(f, fsize))
-			_, werr = w.Write(wdata)
-			check(werr)
-			w.Close()
+func unpackFile(f *os.File, file *FileInfo) []byte {
+	addr := int64(file.Addr)
+	fsize := int(file.Size)
+	f.Seek(addr, 0)
+	file_data := readNumBytes(f, fsize)
+	
+	return file_data
+}
+
+func getPalette(f *os.File, dir_list []*DirectoryInfo, files []*FileInfo, s string) *[]byte {
+	for _, dir := range dir_list {
+		if string(dir.Name[:3]) == "PAL" {
+			fmt.Printf("PAL Format found\n")
+			for _, file := range files[dir.Pos:dir.Pos + dir.Count] {
+				file_name := string(bytes.Trim(file.Name[:12], "x\000"))
+				if file_name == s {
+					fmt.Printf("Unpacking palette: %s\n", file_name)
+					pal := unpackFile(f, file)
+
+					//PAL values are 0x00 - 0x3F, and red/blue channels seem to be swapped
+					for i := 0; i < len(pal); i++ {
+						pal[i] *= 4       
+						if (i+1) % 3 == 0 {    
+							buf := pal[i]
+							pal[i] = pal[i-2]
+							pal[i-2] = buf
+						}
+					}
+					return &pal
+				}
+			}
 		}
 	}
+	log.Fatal("Couldn't find requested PAL file")
+	return nil
 }
 
-func unpack(f *os.File, hdr *Header) {
-	formatList := unpackFormatList(f, hdr)
-	fileList := unpackFileList(f, hdr)
-	unpackFiles(f, hdr, formatList, fileList)
+func unpackFiles(f *os.File, hdr *Header, dir_list []*DirectoryInfo, files []*FileInfo, pal *[]byte)  {
+	var buf_a bytes.Buffer
+	var buf_b bytes.Buffer
+	
+	for _, dir := range dir_list {
+		work_dir := "./" +  string(bytes.Trim(hdr.Name[:12], "x\000")) + "/" +
+			string(dir.Name[:3]) + "/"
+		fmt.Printf("Extracting to %s\n", work_dir)
+		os.MkdirAll(work_dir, os.ModePerm)
+		fmt.Printf("File count: %d\n", dir.Count)
+
+
+		for _, file := range files[dir.Pos:dir.Count + dir.Pos] {
+			s := work_dir + string(bytes.Trim(file.Name[:12], "x\000"))
+			out, err := os.Create(s)
+			check(err)
+	
+			out_data := unpackFile(f, file)
+			
+			if (s[len(s)-3:] == "BMP") {
+				dim := out_data[:4]
+				bmp_x := binary.LittleEndian.Uint16(dim[:2])
+				bmp_y:= binary.LittleEndian.Uint16(dim[2:])
+				bmp_data := out_data[4:]
+				bmp_header := BitmapHeader{
+					HeaderField: 0x4d42,
+					Size: uint32(0x316 + file.Size),
+					DataAddress: 0x316 + 0x9,
+				}
+				
+				bmp_dib := BitmapDIB{
+					DIBSize: 0x0c,
+					Width: bmp_x,
+					Height: bmp_y,
+					ColPlanes: 0x1,
+					Bpp: 0x8,
+				}
+
+				binary.Write(&buf_a, binary.LittleEndian, bmp_header)
+				binary.Write(&buf_b, binary.LittleEndian, bmp_dib)
+				buf_a.Write(buf_b.Next(buf_b.Len()))
+				binary.Write(&buf_b, binary.LittleEndian, bmp_data)
+				buf_a.Write(*pal)
+				buf_a.Write(buf_b.Next(buf_b.Len()))
+				
+				bmp_file := make([]byte, buf_a.Len())
+				err = binary.Read(&buf_a, binary.LittleEndian, bmp_file)
+				check(err)
+				_, err = out.Write(bmp_file)
+				check(err)
+			} else {
+				_, err = out.Write(out_data)
+				check(err)
+			}
+			out.Close()
+		}
+	}
 }
 
 func main() {
@@ -133,7 +215,7 @@ func main() {
 
 	fmt.Printf("%s opened\n", path)
 
-	formatCheck := readNextBytes(f, 1)
+	formatCheck := readNumBytes(f, 1)
 
 	if formatCheck[0] == byte(0x41) {
 		fmt.Printf("Valid RSF format found\n")
@@ -148,7 +230,13 @@ func main() {
 	header := unpackHeader(f, hdrSize)
 
 	fmt.Printf("%s\n%s\n%s\n%s\nFilesize: %d\nFormats: %d Files: %d\n", header.License, header.Name,
-		header.Version, header.Timestamp, header.FileSize, header.FormatCount, header.FileCount)
+		header.Version, header.Timestamp, header.FileSize, header.DirectoryCount, header.FileCount)
 
-	unpack(f, header)
+	directory_list := unpackDirectoryList(f, int(header.DirectoryCount))
+	file_list := unpackFileList(f, int(header.FileCount))
+
+	rgb_pal := getPalette(f, directory_list, file_list, "TRUERGB.PAL")
+	//l23_pal := getPalette(f, header, format_list, file_list, "L23.PAL")
+	
+	unpackFiles(f, header, directory_list, file_list, rgb_pal)
 }
