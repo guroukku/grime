7fcea6b9dee8a0c52266be671a75634ff120d027
Matthew Shaw <mtthwjhnshw@gmail.com>
2017-08-20 22:22:22 +0800

    Changed BMP header format

--- src/rsfunpack.go
+++ src/rsfunpack.go
@@ -8,16 +8,30 @@ import (
 	"os"
 )
 
+type RGB struct {
+	r uint8
+	g uint8
+	b uint8
+}
+
+type RGBA struct {
+	r uint8
+	g uint8
+	b uint8
+	_ uint8
+}
+
 type BitmapHeader struct {
 	HeaderField uint16
 	Size uint32
 	_ uint32
 	DataAddress uint32
 	DIBSize uint32
-	Width uint16
-	Height uint16
+	Width uint32
+	Height uint32
 	ColPlanes uint16
 	Bpp uint16
+	_ [24]byte
 }
 
 type Header struct {
@@ -58,10 +72,10 @@ func check(e error) {
 	}
 }
 
-func insertByte(slice []byte, index uint16, value byte) []byte {
-	s_a := slice[:index]
+func insertByte(slice []byte, index int, value byte) []byte {
+	s_a := slice[:index+1]
 	s_b := slice[index:]
-	s_a = append(s_a, value, value)
+	s_a = append(s_a, value)
 	s_a = append(s_a, s_b...)
 	return s_a
 }
@@ -120,7 +134,7 @@ func unpackFile(f *os.File, file *FileInfo) []byte {
 	return file_data
 }
 
-func getPalette(f *os.File, dir_list []*DirectoryInfo, files []*FileInfo, s string) []byte {
+func getPalette(f *os.File, dir_list []*DirectoryInfo, files []*FileInfo, s string) []*RGB {
 	for _, dir := range dir_list {
 		if string(dir.Name[:3]) == "PAL" {
 			fmt.Printf("PAL directory found\n")
@@ -128,18 +142,19 @@ func getPalette(f *os.File, dir_list []*DirectoryInfo, files []*FileInfo, s stri
 				file_name := string(bytes.Trim(file.Name[:12], "x\000"))
 				if file_name == s {
 					fmt.Printf("Unpacking palette: %s\n", file_name)
-					pal := unpackFile(f, file)
-
-					//PAL values are 0x00 - 0x3F, and red/blue channels seem to be swapped
-					for i := 0; i < len(pal); i++ {
-						pal[i] *= 4       
-						if (i+1) % 3 == 0 {    
-							buf := pal[i]
-							pal[i] = pal[i-2]
-							pal[i-2] = buf
+					palette := make([]*RGB, 256)
+					f.Seek(int64(file.Addr), 0)
+					for i := 0; i < 256; i++ {
+						pal := readNumBytes(f, 3)
+						pal_entry := RGB{
+							r : pal[0],
+							g : pal[1],
+							b : pal[2],
 						}
+						fmt.Printf("%+x\n",pal_entry)
+						palette[i] = &pal_entry
 					}
-					return pal
+					return palette
 				}
 			}
 		}
@@ -148,7 +163,7 @@ func getPalette(f *os.File, dir_list []*DirectoryInfo, files []*FileInfo, s stri
 	return nil
 }
 
-func unpackFiles(f *os.File, hdr *Header, dir_list []*DirectoryInfo, files []*FileInfo, pal []byte)  {
+func unpackFiles(f *os.File, hdr *Header, dir_list []*DirectoryInfo, files []*FileInfo, pal []*RGB)  {
 	var buf bytes.Buffer
 	
 	for _, dir := range dir_list {
@@ -171,31 +186,43 @@ func unpackFiles(f *os.File, hdr *Header, dir_list []*DirectoryInfo, files []*Fi
 			switch file.ID {
 			case 0x200: //Bitmap
 				dim := out_data[:4]
-				bmp_x := binary.LittleEndian.Uint16(dim[:2])
-				bmp_y := binary.LittleEndian.Uint16(dim[2:])
-				bmp_data := out_data[6:]
+				bmp_x := uint32(binary.LittleEndian.Uint16(dim[:2]))
+				bmp_y := uint32(binary.LittleEndian.Uint16(dim[2:]))
+				bmp_data := out_data[4:]
 				bmp_header := BitmapHeader{
 					HeaderField: 0x4d42,
-					Size: uint32(0x31D + file.Size),
-					DataAddress: 0x31D,
-					DIBSize: 0x0c,
+					Size: uint32(0x43B + file.Size),
+					DataAddress: 0x43B,
+					DIBSize: 0x28,
 					Width: bmp_x,
 					Height: bmp_y,
 					ColPlanes: 0x1,
 					Bpp: 0x8,
 				}
 				
-				//Some bitmaps are not 4-byte aligned, so we need to add the padding manually
-				if bmp_x % 4 != 0 {
+				//Some bitmaps are not 4-byte aligned, so we need to pad them manually
+				row := int(bmp_x)
+				if row % 4 != 0 {
 					fmt.Printf("File %s requires padding\n", file.Name)
-					row := uint16(bmp_x)
-					for i := row; i < uint16(len(bmp_data)) - row; i += row {
-						bmp_data = insertByte(bmp_data, i, 0)
+					for i := row; i < len(bmp_data) - row; i += row {
+						for ii := 1; ii < row % 4; ii++ {
+							bmp_data = insertByte(bmp_data, i, 0)
+						}
 					}		
 				}
 				
 				binary.Write(&buf, binary.LittleEndian, bmp_header)
-				buf.Write(pal)
+				
+				//PAL values are 0x00 - 0x3F, and red/blue channels seem to be swapped
+				for i := 0; i < len(pal); i++ {
+					outpal_entry := RGBA{
+						r : pal[i].b * 4,
+						g : pal[i].g * 4,
+						b : pal[i].r * 4, 
+					}
+					binary.Write(&buf, binary.LittleEndian, outpal_entry)
+				}
+				
 				binary.Write(&buf, binary.LittleEndian, bmp_data)
 				
 				bmp_file := make([]byte, buf.Len())
